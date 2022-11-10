9001931f9c4b33a8e9eaea35e9613c281dbdcd84
Song-juni <104546003+Song-juni@users.noreply.github.com>
2022-11-10 20:32:20 +0900

    Add files via upload

---
+++ 6-8.py
@@ -0,0 +1,11 @@
+import turtle
+
+mypen = turtle.Turtle()
+mypen.speed(0)
+mypen.color("#FF0000")
+
+for i in range (1, 10):
+  for i in range (1, 6):
+    mypen.left(144)
+    mypen.forward(200)
+  mypen.left(10)
